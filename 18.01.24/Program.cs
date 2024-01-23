6ce5dee3bcac8585196dac7e076901d1a5a7a4e1
dragoisbg <157458788+dragoisbg@users.noreply.github.com>
2024-01-23 09:36:32 +0200

    Add files via upload

---
+++ 18.01.24/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18._01._24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string listOFBeers = "Amstel,Zgorka,Tuborg,Beks.";
+            string[] beers = listOFBeers.Split(' ', ',', '.');
+            Console.WriteLine("Avaible beers are:");
+            foreach (string beer in beers)
+                Console.WriteLine(beer);
+
+            string az = "Az Dragomir obicham da pia voda";
+            string[] name = az.Split(' ', ',', '.');
+            Console.WriteLine("Avaible az are");
+            Console.WriteLine("zavurshih zashtoto:");
+           
+
+            foreach (string ime in name)
+                Console.WriteLine(ime);
+
+            string replaced = listOFBeers.Replace(",", " ");
+            //vodka and martini and cherry
+
+            Console.WriteLine(replaced);
+
+            string novo = replaced.Remove(9, 4);
+            Console.WriteLine(novo);
+
+            string lowerAlpha = novo.ToLower();
+            Console.WriteLine(lowerAlpha);
+
+            string upperAlpha = az.ToUpper();
+            Console.WriteLine(upperAlpha);
+
+            string novo1 = az.Replace("voda,", " Martini");
+
+
+            string novo2 = az.Remove(27, 4);
+            Console.WriteLine(novo);
+         
+                
+
+             
+
+
+
+        }
+    }
+}
